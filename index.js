2632c65043d5391d4ecc639e2ef2a4e0110d94c7
Preston Darrough <techdarrough@gmail.com>
2022-01-18 20:16:38 -0800

    adding functions

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 function newImage(url, left, bottom){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
+    // image.style.position = 'fixed'
+    // image.style.left = left + 'px'
+    // image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+function move(image, left, bottom){
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+}
 
+let greenCharater = newImage('assets/green-character.gif', 100, 250)
+let tree = newImage('assets/tree.png', 200, 450)
+let pillar = newImage('assets/pillar.png', 350, 250)
+let pineTree = newImage('assets/pine-tree.png', 450, 350)
+let crate = newImage('assets/crate.png', 150, 350)
+let well = newImage('assets/well.png', 500, 575)
+move(greenCharater, 100,250)
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
